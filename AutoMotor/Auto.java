143da1e581082af0865f426e0d7e2488250d8f1e
Marko Livajusic <marko_livajusic0@protonmail.com>
2023-12-18 19:43:32 +0100

    Auto.java: update comment

--- AutoMotor/Auto.java
+++ AutoMotor/Auto.java
@@ -49,8 +49,11 @@ public class Auto {
          * Personen oder Gütern; Kraftwagen, Kraftfahrzeug, Automobil"
          * 
          * Da ein Auto beim Entzug des Motors seinen grundlegenden Sinn, welcher darin besteht, Personen zu transportieren, verliert, habe ich mich hier für die 
-         * Komposition entschieden. Ein Auto ohne Motor ist für mich nur ein "Käfig" aus Metall. Deshalb bin ich der Meinung, dass hier die Komposition
-         * angebrachter ist als die Aggregation. Diese würde aber natürlich auch funktionieren.
+         * Komposition entschieden. Ein Auto ohne Motor ist für mich nur ein "Käfig" aus Metall.
+         * Außerdem: Wenn ein Auto komplett abbrennt, brennt sein Motor damit auch ab
+         * 
+         * Deshalb bin ich der Meinung, dass hier die Komposition angebrachter ist als die Aggregation. Diese würde aber,
+         * was die Implementation angeht, natürlich auch funktionieren.
          */
         motor = new Motor(motorTyp, ps);
     }
