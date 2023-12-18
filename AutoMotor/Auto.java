88834b65f188d8ba40057e50fa87507ecb85eee5
Marko Livajusic <marko_livajusic0@protonmail.com>
2023-12-18 19:19:03 +0100

    Auto.java: remove unneccessary code

--- AutoMotor/Auto.java
+++ AutoMotor/Auto.java
@@ -77,38 +77,3 @@ public class Auto {
     }
 
 }
-public class Auto {
-    private String marke;
-    private int baujahr;
-    private Motor motor;
-
-    public Auto(String marke, int baujahr, String motorTyp, int ps) {
-        this.marke = marke;
-        this.baujahr = baujahr;
-
-        // Komposition
-        motor = new Motor(motorTyp, ps);
-    }
-
-    // Getter und Setter für die Attribute
-    public String getMarke() {
-        return marke;
-    }
-
-    public void setMarke(String marke) {
-        this.marke = marke;
-    }
-
-    public int getBaujahr() {
-        return baujahr;
-    }
-
-    public void setBaujahr(int baujahr) {
-        this.baujahr = baujahr;
-    }
-
-    public Motor getMotor() {
-        return motor;
-    }
-
-}
